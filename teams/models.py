0d6ba043e0cd6e726019250f554b04612cbe0acf
Alex Michael <hi@alexmic.net>
2013-08-31 21:31:30 +0300

    Add team props.

--- teams/models.py
+++ teams/models.py
@@ -8,8 +8,22 @@ class Team(models.Model):
     created = models.DateTimeField(default=now)
     token = models.CharField(max_length=32, default=lambda: uuid.uuid4().hex)
 
+    @property
+    def latest_stage(self):
+        return self.stages.filter(unlocked_on__isnull=False).order_by('problem__order')[0]
+
+    @property
+    def total_points(self):
+        return sum(stage.points_earned for stage in self.stages.all())
+
     def __unicode__(self):
         return self.name
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'token': self.token
+        }
 
 
 class Participant(models.Model):
