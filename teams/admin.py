6641ad4b113c3e310351a3b51abc00923d12cd0c
Alex Michael <hi@alexmic.net>
2013-08-31 21:41:35 +0300

    Admin helper to 'login' an existing team.

--- teams/admin.py
+++ teams/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from .models import Team, Participant
 
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'token', 'created')
+    actions = ['make_active_in_session']
+
+    def make_active_in_session(self, request, queryset):
+        if len(queryset) > 1:
+            self.message_user(request, 'You can only make one team active!')
+        else:
+            team = queryset[0]
+            response = redirect(request.path)
+            response.set_signed_cookie('jhteam', team.token)
+            self.message_user(request, "Team '{}' is now active in this session.".format(team.name))
+            return response
+    make_active_in_session.short_description = 'Make team active in session'
 
 
 class ParticipantAdmin(admin.ModelAdmin):
