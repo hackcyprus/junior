6d725b74428dce51ab914a1c1a8fdc8c30e3390a
Alex Michael <hi@alexmic.net>
2013-08-31 20:22:57 +0300

    Ensure team middleware doesn't blow up if no cookie is present.

--- teams/middleware.py
+++ teams/middleware.py
@@ -7,10 +7,10 @@ class TeamExtractionMiddleware(object):
 
     """
     def process_request(self, request):
-        team_token = request.get_signed_cookie('jhteam')
-        if team_token:
-            try:
+        try:
+            team_token = request.get_signed_cookie('jhteam')
+            if team_token:
                 team = Team.objects.get(token=team_token)
                 request.team = team
-            except Team.DoesNotExist:
-                pass
+        except (Team.DoesNotExist, KeyError):
+            pass
