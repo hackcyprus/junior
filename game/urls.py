22724a7ad663252739bcb428240ad11a14a9f243
Alex Michael <hi@alexmic.net>
2013-08-31 21:30:55 +0300

    Implement map (with bootstrap) and stage endpoints.

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import view_map
+from .views import view_map, view_stage
 
 urlpatterns = patterns('',
     url(r'^map/$', view_map, name='map'),
+    url(r'^stage/(?P<stage_id>\d)$', view_stage, name='view-stage')
 )
--- game/views.py
+++ game/views.py
@@ -1,12 +1,26 @@
 import json
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.functional import curry
+
+from .models import Stage
+from teams.models import Team
+
+
+dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime) else None
+dumps = curry(json.dumps, default=dthandler)
 
 
 class JsonResponse(HttpResponse):
-    def __init__(self, content, status_code=200):
+    def __init__(self, content, status=200):
         content = json.dumps(content)
-        super(JsonResponse, self).__init__(content=content, status_code=status_code)
+        super(JsonResponse, self).__init__(
+            content,
+            status=status,
+            content_type='application/javascript; charset=utf8'
+        )
 
 
 def team_token_required(fn):
@@ -19,20 +33,48 @@ def team_token_required(fn):
         if hasattr(request, 'team') and request.team:
             return fn(*args, **kwargs)
         else:
-            content = {
-                'error': 'You need to register a team to perform this action.'
-            }
-            return JsonResponse(content=content, status_code=403)
+            return JsonResponse(
+                content='You need to register a team to perform this action.',
+                status=403
+            )
     return wrapper
 
 
 def view_map(request):
-    return render(request, 'game/map.html')
+    teams, bootstrap = {}, {}
+    for team in Team.objects.all():
+        teams[team.id] = {
+            'name': team.name,
+            'latest_problem': team.latest_stage.problem.order,
+            'points': team.total_points
+        }
+    bootstrap['teams'] = teams
+    if request.team:
+        bootstrap['team_stages'] = [
+            stage.to_dict() for stage in Stage.objects.filter(team=request.team)
+        ]
+    return render(request, 'game/map.html', {'bootstrap': dumps(bootstrap)})
 
 
 @team_token_required
 def view_stage(request, stage_id):
-    pass
+    try:
+        stage = Stage.objects.get(pk=int(stage_id), team=request.team)
+        if stage.locked:
+            return JsonResponse(content='This stage is locked.', status=403)
+        problem = stage.problem
+        content = {
+            'name': problem.name,
+            'order': problem.order,
+            'description': problem.description,
+            'io_description': problem.io_description,
+            'multiplier': problem.multiplier,
+            'sample_in': problem.sample_in,
+            'sample_out': problem.sample_out
+        }
+        return JsonResponse(content=content)
+    except Stage.DoesNotExist:
+        return JsonResponse(content='Stage not found.', status=404)
 
 
 @team_token_required
@@ -41,5 +83,5 @@ def skip_stage(request):
 
 
 @team_token_required
-def submit_solution(request):
+def submit_stage(request):
     pass
